ed1eaa2ff1424bd9d159f2bd1e096d23db11e559
Tiago Vale <tiago_oliveira914@hotmail.com>
2019-06-11 16:35:44 -0300

    Resolvendo problemas de acentuação

--- src/main/java/com/algaworks/brewer2/config/init/AppInitializer.java
+++ src/main/java/com/algaworks/brewer2/config/init/AppInitializer.java
@@ -1,5 +1,8 @@
 package com.algaworks.brewer2.config.init;
 
+import javax.servlet.Filter;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 import com.algaworks.brewer2.config.WebConfig;
@@ -14,12 +17,20 @@ public class AppInitializer extends AbstractAnnotationConfigDispatcherServletIni
 
 	@Override
 	protected Class<?>[] getServletConfigClasses() {
-		return new Class<?>[] {WebConfig.class};
+		return new Class<?>[] { WebConfig.class };
 	}
 
 	@Override
 	protected String[] getServletMappings() {
-		return new String[] {"/"};
+		return new String[] { "/" };
 	}
 
+	@Override
+	protected Filter[] getServletFilters() {
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		characterEncodingFilter.setForceEncoding(true);
+		
+		return new Filter[] { characterEncodingFilter };
+	}
 }
