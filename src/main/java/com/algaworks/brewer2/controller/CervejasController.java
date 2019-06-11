c2cecb50bc3c5e8bd96fbbcc3df21d49d331ce6c
Tiago Vale <tiago_oliveira914@hotmail.com>
2019-06-11 15:36:05 -0300

    Começando o cadastro de cerveja

--- src/main/java/com/algaworks/brewer2/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer2/controller/CervejasController.java
@@ -2,12 +2,22 @@ package com.algaworks.brewer2.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.algaworks.brewer2.model.Cerveja;
 
 @Controller
 public class CervejasController {
-	
+
 	@RequestMapping("/cervejas/novo")
 	public String novo() {
 		return "cerveja/CadastroCerveja";
+	}
+
+	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
+	public String cadastrar(Cerveja cerveja) {
+		System.out.println("sku: " + cerveja.getSku());
+		return "cerveja/CadastroCerveja";
+
 	}
 }
---
+++ src/main/java/com/algaworks/brewer2/model/Cerveja.java
@@ -0,0 +1,23 @@
+package com.algaworks.brewer2.model;
+
+public class Cerveja {
+
+	private String sku;
+	private String nome;
+
+	public String getSku() {
+		return sku;
+	}
+
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
