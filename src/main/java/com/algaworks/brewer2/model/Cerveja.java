5b0b187dd4fe0f3045a8bfa3cfb917f747d5d50a
Tiago Vale <tiago_oliveira914@hotmail.com>
2019-06-11 16:29:28 -0300

    Desafio: Validando a descrição

--- src/main/java/com/algaworks/brewer2/model/Cerveja.java
+++ src/main/java/com/algaworks/brewer2/model/Cerveja.java
@@ -1,12 +1,20 @@
 package com.algaworks.brewer2.model;
 
+import javax.validation.constraints.Size;
+
 import org.hibernate.validator.constraints.NotBlank;
 
 public class Cerveja {
 
 	@NotBlank
 	private String sku;
+	
+	@NotBlank
 	private String nome;
+	
+	@NotBlank
+	@Size(max=50,message="Tamanho não pode ser maior que 50")
+	private String descricao;
 
 	public String getSku() {
 		return sku;
@@ -22,5 +30,13 @@ public class Cerveja {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
 	}
 }
