28f7d1dab97ad9f381e2803411a19636f28b6482
Tiago Vale <tiago_oliveira914@hotmail.com>
2019-06-12 23:47:30 -0300

    Desafio: prototipando o cadastro de estilo.

---
+++ src/main/java/com/algaworks/brewer2/controller/EstilosController.java
@@ -0,0 +1,14 @@
+package com.algaworks.brewer2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class EstilosController {
+
+	@RequestMapping("/estilos/novo")
+	public String novo() {
+		return "estilo/CadastroEstilo";
+	}
+}
